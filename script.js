7fc52e76154e01c061ff75910620af54dc025e33
Serdar-Web-Master <serdarcholukov@yandex.com>
2022-01-04 19:11:42 +0500

    11

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const call = document.querySelector ('.call');
 console.log(call);
-call.onclick = f1;
+// call.onclick = f1;
 
 function f1() {
 const audio = new Audio();
 audio.src = 'Recording_3.m4a';
 audio.play();
 }
+
+call.addEventListener('click',f1);
