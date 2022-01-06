dbb3aeb5dccd850a170ecb4a8798f06cce8ecc83
Serdar-Web-Master <serdarcholukov@yandex.com>
2022-01-06 20:19:54 +0500

    pop up

--- script.js
+++ script.js
@@ -2,6 +2,37 @@ const wrapper = document.querySelector(".wrapper");
 const display = document.querySelector(".display");
 const footer = document.querySelector(".footer");
 const ok = document.querySelector(".ok");
+const pop__toogle = document.querySelector(".pop__toogle");
+const pop__up = document.querySelector(".pop__up");
+
+// Pop_Up  !!!!!!!!!!!!!!!!
+const input = document.querySelector(".input");
+const button = document.querySelector(".button");
+
+pop__toogle.addEventListener("click", () => {
+  pop__up.classList.add("dis__on");
+});
+
+let balans;
+
+button.onclick = inputReding;
+
+function inputReding() {
+  balans = input.value;
+  console.log(balans);
+}
+
+console.log(balans);
+// let balans = 0;
+
+// let vyvod = button.addEventListener("click", () => {
+//   balans = input.value;
+//   return balans;
+// });
+
+// console.log(vyvod);
+
+// Pop Up  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 const call = document.querySelector(".call");
 // console.log(call);
